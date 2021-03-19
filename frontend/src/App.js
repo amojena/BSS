8d6364db498c21b8a66fc0dde1b52b1006194d84
amojena <am3238@cornell.edu>
2021-03-18 21:44:20 -0400

    reworked landing page so that it is not a sign in page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,49 +4,54 @@ function App() {
   return (
     <div className="App">
 
-      <div className="hor-center">
-        <div className="column is-half">
-          <div className="custom-top-pad">
-            <div class="control">
+      <div class="box">
+
+        <h1 class="title">Live Locally</h1>
+
+        <div class="custom-top-pad">
+          <div class="control">
                 <input class="input is-rounded is-large" type="text" placeholder="🔍 Where to next?"></input>
-                </div>
           </div>
         </div>
-      </div>
 
-      <div className= "hor-center">
-        <div className= "column is-one-third">
-          <form class="box">
-            <div class="field">
-              <label class="label">Email</label>
-              <div class="control">
-              <input class="input" type="email" placeholder="e.g. alex@example.com"></input>
+        <div className="subtitle-top-pad">
+          <h2 class="subtitle">Tell us where you're going next and we'll find you a local who can help you during your travel!</h2>
+        </div>
+          
+        <div class="custom-top-pad">
+          <div class="columns is one-third">
+              <div class="column">
+                <button class="button is-success is-light">Log In</button>
+              </div>
+              <div class="column">
+                <button class="button is-link is-light">Sign Up</button>
               </div>
             </div>
 
-            <div class="field">
-            <label class="label">Password</label>
-            <div class="control">
-              <input class="input" type="password" placeholder="********"></input>
-            </div>
-          </div>
-
-        <div class="columns is one-quarter">
-          <div class="column">
-            <button class="button is-success is-light">Log In</button>
-          </div>
-          <div class="column">
-            <button class="button is-link is-light">Sign Up</button>
-          </div>
         </div>
+          
 
-          </form>
-        </div>
+        
+
       </div>
-
 
     </div>
   );
 }
 
 export default App;
+
+
+{/* <div class="field">
+            <label class="label">Email</label>
+            <div class="control">
+              <input class="input" type="email" placeholder="e.g. alex@example.com"></input>
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input class="input" type="password" placeholder="********"></input>
+            </div>
+          </div> */}
