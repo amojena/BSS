9ab74addf8a654d35853446c8ac81e18556f0e5d
amojena <am3238@cornell.edu>
2021-03-18 20:49:14 -0400

    Landing page done. Need to make button react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-import background from "./img/bg3.jpg"
 
 function App() {
   return (
-    <div className="App" >
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        <code>Edit src/App.js and save to reload.</code>
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
+    <div className="App">
+
+      <div className="hor-center">
+        <div className="column is-half">
+          <div className="custom-top-pad">
+            <div class="control">
+                <input class="input is-rounded is-large" type="text" placeholder="🔍 Where to next?"></input>
+                </div>
+          </div>
+        </div>
+      </div>
+
+      <div className= "hor-center">
+        <div className= "column is-one-third">
+          <form class="box">
+            <div class="field">
+              <label class="label">Email</label>
+              <div class="control">
+              <input class="input" type="email" placeholder="e.g. alex@example.com"></input>
+              </div>
+            </div>
+
+            <div class="field">
+            <label class="label">Password</label>
+            <div class="control">
+              <input class="input" type="password" placeholder="********"></input>
+            </div>
+          </div>
+
+        <div class="columns is one-quarter">
+          <div class="column">
+            <button class="button is-success is-light">Log In</button>
+          </div>
+          <div class="column">
+            <button class="button is-link is-light">Sign Up</button>
+          </div>
+        </div>
+
+          </form>
+        </div>
+      </div>
+
+
     </div>
   );
 }
