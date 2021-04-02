23c1e287173d2de7c298a8f0ccf69fc2537c6310
amojena <am3238@cornell.edu>
2021-04-01 23:14:17 -0400

    forgot to do git commit from root directory

--- backend/api.js
+++ backend/api.js
@@ -1,19 +1,38 @@
 'use strict';
 
+
 module.exports.getTravelReqs = async (event) => {
+  
+  const firebaseTokenVerifier = require('../node_modules/firebase-token-verifier')
+  const projectId = "bss-ll"
+
   if (event.httpMethod === "GET" && event.path === "/requests") {
+
+    const token = event.headers['Authorization']
+
+    // no authorization token provided, return error code
+    if (!token) {
+      console.log("No token")
+      return {statusCode: 401}
+    }
+
+    //validate token from request
+    try{
+      const decoded = await firebaseTokenVerifier.validate(token,projectId)
+    } catch(err) {
+      // invalid token
+      console.error(err)
+      return {statusCode: 401}
+    }
+    
+
+
     return {
       statusCode: 200,
       body: JSON.stringify({ name: 'Antonio Mojena', location: "Puerto Rico", status: "Pending" })
     }
-  }
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ method: event.httpMethod, pth: event.path })
+
   }
 
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
 
